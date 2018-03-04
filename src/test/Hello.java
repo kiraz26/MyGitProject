82542fcc680dd470cfcba71a56621901e307b3a9
greys26 <talhaerturk@gmail.com>
2018-03-04 17:19:59 -0500

    talha update

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -4,5 +4,15 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Naber?");
 		System.out.println("iyi senden?");
+		
+		System.out.println("iyi senden?");
+		System.out.println("iyi senden?");
+		System.out.println("iyi senden?");
+		System.out.println("iyi senden?");
+		System.out.println("iyi senden?");
+		System.out.println("iyi senden?");
+
+		
+		
 	}
 }
